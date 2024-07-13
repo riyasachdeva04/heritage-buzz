47b954e761120b4d691fa951eaf7dc7662290c08
riyasachdeva04 <riiya2048@gmail.com>
2024-07-13 23:08:56 +0530

    logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,40 @@
-
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { useEffect, useState} from "react";
 import SignupPage from "./pages/signup";
 import SigninPage from "./pages/signin";
 
 import './App.css';
+import { app } from "./firebase";
 
+const auth = getAuth(app);
 
 function App() {
 
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, [])
+
+  if (user === null){
+    return (
+      <div classname="App">
+        <SigninPage/>
+        <SignupPage/>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <SignupPage/>
-      <SigninPage/>
+      <h1>Hi {user.email}</h1>
+      <button onClick={() => {signOut(auth)}}>Logout</button>
     </div>
   );
 }
