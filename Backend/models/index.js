dfba54c9677c1b12c661bc4c556917db30fb9a85
riyasachdeva04 <riiya2048@gmail.com>
2024-07-29 16:14:06 +0530

    Remove frontend directory from repository but keep it locally

--- Backend/models/index.js
+++
@@ -1,7 +0,0 @@
-const User = require('./user.js');
-const Request = require('./request.js');
-
-User.hasMany(Request, { foreignKey: 'userId' });
-Request.belongsTo(User, { foreignKey: 'userId' });
-
-module.exports = { User, Request };
